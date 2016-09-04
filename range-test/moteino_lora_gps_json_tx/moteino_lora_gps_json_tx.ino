ac0c90964a122482e038fd98a4b5125dbaa1ff0d
dwblair <donblair@gmail.com>
2016-09-04 10:45:23 -0400

    update

---
+++ range-test/moteino_lora_gps_json_tx/moteino_lora_gps_json_tx.ino
@@ -0,0 +1,284 @@
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+
+// Send voltage
+// https://learn.adafruit.com/adafruit-feather-m0-adalogger/power-management
+ 
+#include <SPI.h>
+#include <RH_RF95.h>
+#include "LowPower.h"
+#include <ArduinoJson.h>
+
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+
+
+// Device ID
+#define DEVID 8
+
+#define sleep_8_loops 1 // number of sleep loops 
+
+
+// GPS
+// If using software serial, keep this line enabled
+// (you can change the pin numbers to match your wiring):
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
+#define GPSECHO  false
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+
+
+// LORA
+
+// Change to 434.0 or other frequency, must match RX's freq!
+//#define RF95_FREQ 915.0
+#define RF95_FREQ 868.0
+
+
+// Singleton instance of the radio driver
+//RH_RF95 rf95(RFM95_CS, RFM95_INT);
+RH_RF95 rf95;
+
+// Blinky on receipt
+#define LED 9
+
+// Voltage PIN
+#define VBATPIN A7
+
+
+void setup() 
+{
+
+  // GPS -------------------------------------------------
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+
+  
+
+  
+  pinMode(LED, OUTPUT);
+  //pinMode(RFM95_RST, OUTPUT);
+  //digitalWrite(RFM95_RST, HIGH);
+ 
+//  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+ 
+  Serial.println(" LoRa TX Test!");
+ 
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+ 
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+ 
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+ 
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+uint32_t timer = millis();
+
+
+void loop()
+{ 
+
+// GPS
+
+ // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+
+Serial.print("Location: ");
+
+/*
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+   */
+
+      /*
+       Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", "); 
+      Serial.println(GPS.longitudeDegrees, 4);
+     */
+
+
+  // LORA radio
+  
+  Serial.println("Sending to rf95_server");
+
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["id"] = DEVID;
+  root["millis"] = millis();
+  JsonObject& data = root.createNestedObject("data");
+  data["vbat"] = double_with_n_digits(measuredvbat, 3);
+  data["lat"] = double_with_n_digits(GPS.latitudeDegrees,6);
+  data["lon"] = double_with_n_digits(GPS.longitudeDegrees,6);
+/*
+  data["s_a_t"] = double_with_n_digits(s_a_t, 2);
+  s_a_h
+  
+  s_b_t
+  s_b_h
+  
+  s_c_t
+  s_c_h
+  
+  t_1
+
+  t_1
+
+*/
+  char buf[251];
+  root.printTo(buf, sizeof(buf));
+  buf[sizeof(buf)-1] = 0;
+  
+  Serial.print("Sending "); Serial.println(buf);
+  rf95.send((uint8_t *)buf, sizeof(buf));
+
+ 
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+
+/*
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+ 
+  Serial.println("Waiting for reply..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+*/
+
+    digitalWrite(LED,HIGH);
+    delay(200);
+    digitalWrite(LED, LOW);
+    
+  //delay(8000);
+  
+  delay(2000);
+
+  /*
+  for (int i=0;i<sleep_8_loops;i++) {
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
+  }
+  */
+  
+}
